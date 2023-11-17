84bb8293b683627410ff12460427f1a735cc5d2c
Hossein Khosravipour <hossein.kh7935@gmail.com>
2023-11-17 14:12:11 +0330

    change model type

--- going_modular/model.py
+++ going_modular/model.py
@@ -5,21 +5,48 @@ import torch
 class MLP(nn.Module):
     def __init__(self, in_features, num_classes, list_hidden_features_size):
         super().__init__()
-        # self.num_hidden_layer = num_hidden_layer
 
         self.input_layer = nn.Linear(in_features, list_hidden_features_size[0])
-        
+        self.batch_norm_input = nn.BatchNorm1d(list_hidden_features_size[0])
+
         self.hidden_layers = nn.ModuleList(
-            [nn.Linear(list_hidden_features_size[i], list_hidden_features_size[i+1]) for i in range(len(list_hidden_features_size)-1)]
+            [nn.Sequential(
+                nn.Linear(list_hidden_features_size[i], list_hidden_features_size[i + 1]),
+                nn.BatchNorm1d(list_hidden_features_size[i + 1]),
+                nn.ReLU(inplace=True)
+            ) for i in range(len(list_hidden_features_size) - 1)]
         )
 
         self.out_layer = nn.Linear(list_hidden_features_size[-1], num_classes)
 
     def forward(self, x):
-        x = torch.relu(self.input_layer(x)) 
+        x = self.batch_norm_input(torch.relu(self.input_layer(x)))
+
         for layer in self.hidden_layers:
-            x = torch.relu(layer(x))
+            x = layer(x)
 
         x = self.out_layer(x)
+
+        return x
+    # def __init__(self, in_features, num_classes, list_hidden_features_size):
+    #     super().__init__()
+
+    #     # self.num_hidden_layer = num_hidden_layer
+
+    #     self.input_layer = nn.Linear(in_features, list_hidden_features_size[0])
+        
+    #     self.hidden_layers = nn.ModuleList(
+    #         [nn.Linear(list_hidden_features_size[i], list_hidden_features_size[i+1]) for i in range(len(list_hidden_features_size)-1)]
+    #     )
+
+    #     self.out_layer = nn.Linear(list_hidden_features_size[-1], num_classes)
+
+    # def forward(self, x):
+    #     x = torch.relu(self.input_layer(x)) 
+    #     for layer in self.hidden_layers:
+    #         x = torch.relu(layer(x))
+    #         # x = torch.relu(torch.batch_norm(layer(x)))
+
+    #     x = self.out_layer(x)
         
         return x
